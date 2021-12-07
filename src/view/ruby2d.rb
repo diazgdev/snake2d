1dcf2d666cc49e5ee50fb02be00de866958cb62b
Guillermo Diaz <diazgdev@gmail.com>
2021-12-07 13:45:21 -0600

    added sample snake and food to canvas

--- src/view/ruby2d.rb
+++ src/view/ruby2d.rb
@@ -4,12 +4,41 @@ module View
   class Ruby2dView
 
     def initialize
+      @pixel_size = 50
     end
 
     def render(state)
       extend Ruby2D::DSL
-      set(width: 640, height: 480, title: "Snake", fps: 60, vsync: true)
+      set(width: @pixel_size * state.grid.cols, height: @pixel_size * state.grid.rows, title: 'Snake', fps: 60, vsync: true)
+      render_food(state)
+      render_snake(state)
       show
+    end
+
+    private
+
+    def render_food(state)
+      extend Ruby2D::DSL
+      food = state.food
+      Square.new(
+        x: food.col * @pixel_size,
+        y: food.row * @pixel_size,
+        size: @pixel_size,
+        color: 'yellow'
+      )
+    end
+
+    def render_snake(state)
+      extend Ruby2D::DSL
+      snake = state.snake
+      snake.positions.each do |pos|
+        Square.new(
+          x: pos.col * @pixel_size,
+          y: pos.row * @pixel_size,
+          size: @pixel_size,
+          color: 'green'
+        )
+      end
     end
   end
 end
