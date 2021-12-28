7f1a6e3d61f4a3f9c365f5d9602a1dfe6cefc446
Guillermo Diaz <diazgdev@gmail.com>
2021-12-28 14:24:52 -0600

    added minitest gem and tested the actions file

--- Gemfile
+++ Gemfile
@@ -6,4 +6,5 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
 
-gem 'ruby2d'
+gem 'ruby2d', '~> 0.11.1'
+gem 'minitest', '~> 5.15'
--- src/actions/actions.rb
+++ src/actions/actions.rb
@@ -3,7 +3,7 @@ module Actions
     next_direction = state.next_direction
     next_position = calc_next_position(state)
     # verify if the next position is valid
-    if position_is_valid?(state)
+    if position_is_valid?(state, next_position)
       move_snake_to(state, next_position)
     else
       end_game(state)
@@ -12,29 +12,29 @@ module Actions
 
   private
 
-  def calc_next_position(state)
+  def self.calc_next_position(state)
     current_position = state.snake.positions.first
     case state.next_direction
-      when UP
+      when Model::Direction::UP
         return Model::Coord.new(
           current_position.row - 1,
           current_position.col)
-      when RIGHT
+      when Model::Direction::RIGHT
         return Model::Coord.new(
           current_position.row,
           current_position.col + 1)
-      when DOWN
+      when Model::Direction::DOWN
         return Model::Coord.new(
           current_position.row + 1,
           current_position.col)
-      when LEFT
+      when Model::Direction::LEFT
         return Model::Coord.new(
           current_position.row,
           current_position.col - 1)
     end
   end
 
-  def position_is_valid?(state, position)
+  def self.position_is_valid?(state, position)
     is_invalid = ((position.row >= state.grid.rows ||
       position.row < 0 ) ||
       (position.col >= state.grid.cols ||
@@ -44,13 +44,13 @@ module Actions
     return !(state.snake.positions.include? position)
   end
 
-  def move_snake_to(state, next_position)
-    new positions = [next_position] + state.snake.positions[0...-1]
+  def self.move_snake_to(state, next_position)
+    new_positions = [next_position] + state.snake.positions[0...-1]
     state.snake.positions = new_positions
     state
   end
 
-  def end_game(state)
+  def self.end_game(state)
     state.game_finished = true
     state
   end
